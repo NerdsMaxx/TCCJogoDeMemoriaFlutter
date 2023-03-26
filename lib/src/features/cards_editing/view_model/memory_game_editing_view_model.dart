0771546b0a3ba740a3e63a4ed6cfafef84637f7b
Guilherme Henrique <henrique101124@gmail.com>
2023-03-26 15:36:28 -0300

    Salvando antes

--- lib/src/features/cards_editing/view_model/memory_game_editing_view_model.dart
+++ lib/src/features/cards_editing/view_model/memory_game_editing_view_model.dart
@@ -1,3 +1,25 @@
-class MemoryGameEditingViewModel { 
+import 'dart:js';
 
+import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
+
+class MemoryGameEditingViewModel {
+  static late final MemoryGameEditingViewModel instance;
+  
+  static dispose() {
+    instance.formKeyCardEditing.currentState!.dispose();
+  }
+
+  MemoryGameEditingViewModel(this.context) {
+    instance = this;
+  }
+
+  
+
+  final BuildContext context;
+
+  final GlobalKey<FormState> formKeyCardEditing = GlobalKey<FormState>();
+  final validatorContent = ValidationBuilder(requiredMessage: 'É obrigatório escrever um conteúdo!')
+      .minLength(1, 'Deve ter no mínimo um caracter')
+      .build();
 }
